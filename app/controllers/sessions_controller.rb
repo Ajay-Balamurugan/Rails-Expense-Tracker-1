d752e29f63cb91c6c60a518ec705f9f4a79c8671
Ajay Balamurugan <121103098+Ajay-Balamurugan@users.noreply.github.com>
2024-03-17 17:07:22 +0530

    added home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,6 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id
             redirect_to root_path, notice: "Logged in successfully"
         else
-            puts "Pda Punda"
             flash[:alert] = "Invalid email or password"
             redirect_to login_path
         end
